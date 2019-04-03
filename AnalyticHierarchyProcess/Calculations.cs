47d00ce17cd5b09bbd3ecbc44cb4be94fdec661a
Dmitry <sangvinion1@gmail.com>
2019-04-03 12:57:15 +0300

    starting SOLE

--- AnalyticHierarchyProcess/Calculations.cs
+++ AnalyticHierarchyProcess/Calculations.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Task;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace AnalyticHierarchyProcess
 {
     public class Calculations
     {
-        GetIndexAgreed();
+        public Calculations(Task.Task task) {
+            task.GetIndexAgreed();
+
+        }
+
+        public int CalcCR() {
+            return 0;
+        }
+
+        public int[] Sole(int[] matrix) {
+            SparseMatrix matrix1 = new SparseMatrix(n);
+            //matrix1.Solve
+            int[] a = new int[5];
+            return new int[7];
+        }
     }
 }
