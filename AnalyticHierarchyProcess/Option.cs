ffd69df3f5548e8ac10f4166555fe5141de78a28
Роман Соловьёв <roman.soloweow333@ya.ru>
2019-04-03 12:53:35 +0300

    update all

--- AnalyticHierarchyProcess/Option.cs
+++ AnalyticHierarchyProcess/Option.cs
@@ -6,8 +6,8 @@ namespace Option
     {
         public string name;
         double globalPriority = -1;
-        Vector<double> Priority
-        Option(string _name, List<string> _criterions):base(_criterions)
+    
+        Option(string _name, List<string> _criterions)
         {
             name = _name;
         }
