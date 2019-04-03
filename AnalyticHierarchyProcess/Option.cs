71c74d9ce0d8213c88bad67ce535506fa0ba385a
Роман Соловьёв <roman.soloweow333@ya.ru>
2019-04-03 12:53:35 +0300

    update all

---
+++ AnalyticHierarchyProcess/Option.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+namespace Option
+{
+    class Option
+    {
+        public string name;
+        double globalPriority = -1;
+    
+        Option(string _name, List<string> _criterions)
+        {
+            name = _name;
+        }
+    }
+}
