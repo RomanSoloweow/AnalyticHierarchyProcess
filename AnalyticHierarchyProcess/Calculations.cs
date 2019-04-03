e353c9b0ca14fba063923bbea71f9d67fc26ea06
Dmitry <sangvinion1@gmail.com>
2019-04-03 14:51:24 +0300

    building SOLE (System of linear equations)

--- AnalyticHierarchyProcess/Calculations.cs
+++ AnalyticHierarchyProcess/Calculations.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Task;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace AnalyticHierarchyProcess
 {
@@ -20,11 +21,54 @@ namespace AnalyticHierarchyProcess
             return 0;
         }
 
-        public int[] Sole(int[] matrix) {
-            SparseMatrix matrix1 = new SparseMatrix(n);
-            //matrix1.Solve
-            int[] a = new int[5];
-            return new int[7];
+        public void Sole(Matrix<double> _matrix) {
+
+            /*
+                на вход подается матрица
+                вычисляем ее максимальное собственное значение
+                из главной диагонали матрицы вычитаем это собственное значение
+                создаем вектор, необходимый для решения слау
+                изменяем матрицу для решения слау
+                решаем слау 
+            */
+
+            // максимальное собственное значение
+            double MaxEigenVal = MaxEigenValue(_matrix);
+
+            //из главной диагонали матрицы вычитаем это собственное значение
+            for (int i=0; i < _matrix.RowCount; i++) {
+                for (int j = 0; j < _matrix.ColumnCount; j++)
+                {
+                    if (i == j)
+                        _matrix[i, j] = MaxEigenVal;
+                }
+            }
+
+            //создаем вектор, необходимый для решения слау
+            var _vector = Vector<double>.Build.Dense(_matrix.RowCount-1);
+            _vector = _matrix.Column(0)*(-1);
+
+
+
+            double[,] data = new double[,]
+        {
+            {  2,  0,  3},
+            { 10, -3, -6},
+            { -1,  0, -2}
+        };
+
+            Matrix<double> matrix = DenseMatrix.OfArray(data);
+            Console.WriteLine(matrix);
+
+            var eigen = matrix.Evd();
+            var values = eigen.EigenValues.Enumerate().GetEnumerator();
+            var vectors = eigen.EigenVectors.EnumerateColumns().GetEnumerator();
+
+            while (vectors.MoveNext() && values.MoveNext())
+            {
+                Console.WriteLine("Собственный вектор при {0}:\n{1}",
+                    values.Current, vectors.Current);
+            }
         }
     }
 }
